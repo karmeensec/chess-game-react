7230066f3519e3438cf63334550b703a99b6950a
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2024-03-01 00:07:08 +0100

    piece array declaration

--- src/components/Chessboard/Chessboard.tsx
+++ src/components/Chessboard/Chessboard.tsx
@@ -1,19 +1,27 @@
-import React from "react";
 import "./Chessboard.css";
 import Tile from "../Tile/Tile";
+import king from "../../assets/images/king.png";
 
 const horizontalAxis = ["a", "b", "c", "d", "e", "f", "g", "h"];
 const verticalAxis = ["1", " 2", " 3", " 4", " 5", "6", " 7", " 8"];
 
+interface Piece {
+  image: string;
+  x: number;
+  y: number;
+}
+
+const pieces: Piece[] = [];
+
 const Chessboard = () => {
-  let board = [];
+  const board = [];
 
   for (let j = verticalAxis.length - 1; j >= 0; j--) {
     for (let i = 0; i < horizontalAxis.length; i++) {
       const startZero = 2;
       const number = i + j + startZero;
 
-      board.push(<Tile number={number} />);
+      board.push(<Tile number={number} image={king} />);
     }
   }
 
