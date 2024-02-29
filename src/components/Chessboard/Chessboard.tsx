e08e606a14a7773911492203e7711cf854969d67
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2024-02-29 23:46:33 +0100

    update file structure

--- src/components/Chessboard/Chessboard.tsx
+++ src/components/Chessboard/Chessboard.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Chessboard.css";
+import Tile from "../Tile/Tile";
 
 const horizontalAxis = ["a", "b", "c", "d", "e", "f", "g", "h"];
 const verticalAxis = ["1", " 2", " 3", " 4", " 5", "6", " 7", " 8"];
@@ -12,19 +13,7 @@ const Chessboard = () => {
       const startZero = 2;
       const number = i + j + startZero;
 
-      if (number % 2 === 0) {
-        board.push(
-          <div className="tile black-tile ">
-            {horizontalAxis[i]} {verticalAxis[j]}
-          </div>
-        );
-      } else {
-        board.push(
-          <div className="tile white-tile ">
-            {horizontalAxis[i]} {verticalAxis[j]}
-          </div>
-        );
-      }
+      board.push(<Tile number={number} />);
     }
   }
 
