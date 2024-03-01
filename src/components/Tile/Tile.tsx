0fa3f929f8dcbafc6130d58136684457ad1eec82
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2024-03-01 08:56:34 +0100

    converting piece images to the background image

--- src/components/Tile/Tile.tsx
+++ src/components/Tile/Tile.tsx
@@ -9,13 +9,23 @@ const Tile = ({ number, image }: TileProps) => {
   if (number % 2 === 0) {
     return (
       <div className="tile black-tile ">
-        <img src={image} alt="" />
+        {image && (
+          <div
+            style={{ backgroundImage: `url("${image}")` }}
+            className="chess-piece"
+          ></div>
+        )}
       </div>
     );
   } else {
     return (
       <div className="tile white-tile ">
-        <img src={image} alt="" />
+        {image && (
+          <div
+            style={{ backgroundImage: `url("${image}")` }}
+            className="chess-piece"
+          ></div>
+        )}
       </div>
     );
   }
