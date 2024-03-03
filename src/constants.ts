04151f55c795d84a4e5e7683e87adaaf685abfc3
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2024-03-03 20:21:59 +0100

    organize code

--- src/constants.ts
+++ src/constants.ts
@@ -1,13 +1,9 @@
 export const horizontalAxis = ["a", "b", "c", "d", "e", "f", "g", "h"];
 export const verticalAxis = ["1", " 2", " 3", " 4", " 5", "6", " 7", " 8"];
 
-export interface Piece {
-  image: string;
+export interface Position {
   x: number;
   y: number;
-  type: PieceType;
-  team: TeamType;
-  enPassant?: boolean;
 }
 
 export enum PieceType {
@@ -22,6 +18,14 @@ export enum PieceType {
 export enum TeamType {
   OPPONENT,
   MY,
+}
+
+export interface Piece {
+  image: string;
+  position: Position;
+  type: PieceType;
+  team: TeamType;
+  enPassant?: boolean;
 }
 
 export const initialBoardState: Piece[] = [];
