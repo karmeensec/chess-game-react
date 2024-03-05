99d3fe7b60101aee4a5a48ed7c75492e27ffb71b
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2024-03-05 16:10:11 +0100

    apply the same queen movement to the king with 2 iterations

--- src/components/referee/Referee.ts
+++ src/components/referee/Referee.ts
@@ -350,6 +350,40 @@ export default class Referee {
     team: TeamType,
     boardState: Piece[]
   ): boolean {
+    for (let i = 1; i < 2; i++) {
+      // Diagonal
+
+      const multiplierX =
+        desiredPosition.x < initialPosition.x
+          ? -1
+          : desiredPosition.x > initialPosition.x
+          ? 1
+          : 0;
+      const multiplierY =
+        desiredPosition.y < initialPosition.y
+          ? -1
+          : desiredPosition.y > initialPosition.y
+          ? 1
+          : 0;
+
+      const passedPosition: Position = {
+        x: initialPosition.x + i * multiplierX,
+        y: initialPosition.y + i * multiplierY,
+      };
+
+      if (samePosition(passedPosition, desiredPosition)) {
+        if (
+          this.isTileEmptyOrOccupiedByEnemy(passedPosition, boardState, team)
+        ) {
+          return true;
+        }
+      } else {
+        if (this.isTileOccupied(passedPosition, boardState)) {
+          break;
+        }
+      }
+    }
+
     return false;
   }
 
