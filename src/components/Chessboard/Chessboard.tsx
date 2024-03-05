27fbc6d6052b2b12a0b1e0eb97e5c1e1106af5c6
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2024-03-05 20:30:11 +0100

    console check for the piece if up for promotion

--- src/components/Chessboard/Chessboard.tsx
+++ src/components/Chessboard/Chessboard.tsx
@@ -368,14 +368,25 @@ const Chessboard = () => {
 
           const updatedPieces = pieces.reduce((results, piece) => {
             if (samePosition(piece.position, grabPosition)) {
+              console.log("Grabbed piece", piece);
+
               piece.enPassant =
                 Math.abs(grabPosition.y - y) === 2 &&
                 piece.type === PieceType.PAWN;
 
               piece.position.x = x;
               piece.position.y = y;
+
+              const promotionRow = piece.team === TeamType.MY ? 7 : 0;
+
+              if (y === promotionRow) {
+                console.log("Up for promotion row");
+              }
+
               results.push(piece);
             } else if (!samePosition(piece.position, { x, y })) {
+              console.log("Not Grabbed piece", piece);
+
               if (piece.type === PieceType.PAWN) {
                 piece.enPassant === false;
               }
