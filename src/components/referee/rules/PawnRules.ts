48ff2898f84623c235d19df070f896cf5cf6b000
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2024-03-05 17:05:22 +0100

    fix import

---
+++ src/components/referee/rules/PawnRules.ts
@@ -0,0 +1,62 @@
+import { Piece, Position, TeamType } from "../../../constants";
+import { isTileOccupied, isTileOccupiedByEnemy } from "./GeneralRules.ts";
+
+export const pawnMovement = (
+  initialPosition: Position,
+  desiredPosition: Position,
+  team: TeamType,
+  boardState: Piece[]
+): boolean => {
+  const specialRow = team === TeamType.MY ? 1 : 6;
+  const pawnDirection = team === TeamType.MY ? 1 : -1;
+
+  // Pawn Movement
+
+  if (
+    initialPosition.x === desiredPosition.x &&
+    initialPosition.y === specialRow &&
+    desiredPosition.y - initialPosition.y === 2 * pawnDirection
+  ) {
+    if (
+      !isTileOccupiedByEnemy(desiredPosition, boardState, team) &&
+      !isTileOccupied(
+        { x: desiredPosition.x, y: desiredPosition.y - pawnDirection },
+        boardState
+      )
+    ) {
+      return true;
+    }
+  }
+  if (
+    initialPosition.x === desiredPosition.x &&
+    desiredPosition.y - initialPosition.y === pawnDirection
+  ) {
+    if (!isTileOccupied(desiredPosition, boardState)) {
+      return true;
+    }
+  }
+
+  // Pawn Attacking
+  if (
+    desiredPosition.x - initialPosition.x === -1 &&
+    desiredPosition.y - initialPosition.y === pawnDirection
+  ) {
+    // Upper or Bottom Left Attacking
+
+    if (isTileOccupiedByEnemy(desiredPosition, boardState, team)) {
+      return true;
+    }
+  }
+  if (
+    desiredPosition.x - initialPosition.x === 1 &&
+    desiredPosition.y - initialPosition.y === pawnDirection
+  ) {
+    // Upper or Bottom Right Attacking
+
+    if (isTileOccupiedByEnemy(desiredPosition, boardState, team)) {
+      return true;
+    }
+  }
+
+  return false;
+};
