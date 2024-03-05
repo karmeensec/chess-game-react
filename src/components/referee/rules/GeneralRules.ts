cbbef2f0f4630b3110a6287c7ae6a127ecdb7171
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2024-03-05 17:05:42 +0100

    isolate to general rules files

---
+++ src/components/referee/rules/GeneralRules.ts
@@ -0,0 +1,30 @@
+import { Piece, Position, TeamType, samePosition } from "../../../constants";
+
+export const isTileOccupied = (
+  position: Position,
+  boardState: Piece[]
+): boolean => {
+  const piece = boardState.find((p) => samePosition(p.position, position));
+
+  if (piece) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+export const isTileOccupiedByEnemy = (
+  position: Position,
+  boardState: Piece[],
+  team: TeamType
+): boolean => {
+  const piece = boardState.find(
+    (p) => samePosition(p.position, position) && p.team !== team
+  );
+
+  if (piece) {
+    return true;
+  } else {
+    return false;
+  }
+};
