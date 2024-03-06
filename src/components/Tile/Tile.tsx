2cd788b4f125f68e32729932a69cf97786348173
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2024-03-06 22:02:32 +0100

    refactor classnames

--- src/components/Tile/Tile.tsx
+++ src/components/Tile/Tile.tsx
@@ -3,32 +3,29 @@ import "./Tile.css";
 interface TileProps {
   number: number;
   image: string;
+  highlight: boolean;
 }
 
-const Tile = ({ number, image }: TileProps) => {
-  if (number % 2 === 0) {
-    return (
-      <div className="tile black-tile ">
-        {image && (
-          <div
-            style={{ backgroundImage: `url("${image}")` }}
-            className="chess-piece"
-          ></div>
-        )}
-      </div>
-    );
-  } else {
-    return (
-      <div className="tile white-tile ">
-        {image && (
-          <div
-            style={{ backgroundImage: `url("${image}")` }}
-            className="chess-piece"
-          ></div>
-        )}
-      </div>
-    );
-  }
+const Tile = ({ number, image, highlight }: TileProps) => {
+  const className: string = [
+    "tile",
+    number % 2 === 0 && "black-tile",
+    number % 2 !== 0 && "white-tile",
+    highlight && "tile-highlight",
+  ]
+    .filter(Boolean)
+    .join(" ");
+
+  return (
+    <div className={className}>
+      {image && (
+        <div
+          style={{ backgroundImage: `url("${image}")` }}
+          className="chess-piece"
+        ></div>
+      )}
+    </div>
+  );
 };
 
 export default Tile;
