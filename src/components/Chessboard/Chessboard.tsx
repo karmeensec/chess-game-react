6aa71ac71a01695156e95b782b6aac13bb18afc7
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2024-03-06 16:19:01 +0100

    remove console logs

--- src/components/Chessboard/Chessboard.tsx
+++ src/components/Chessboard/Chessboard.tsx
@@ -371,8 +371,6 @@ const Chessboard = () => {
 
           const updatedPieces = pieces.reduce((results, piece) => {
             if (samePosition(piece.position, grabPosition)) {
-              console.log("Grabbed piece", piece);
-
               piece.enPassant =
                 Math.abs(grabPosition.y - y) === 2 &&
                 piece.type === PieceType.PAWN;
@@ -389,8 +387,6 @@ const Chessboard = () => {
 
               results.push(piece);
             } else if (!samePosition(piece.position, { x, y })) {
-              console.log("Not Grabbed piece", piece);
-
               if (piece.type === PieceType.PAWN) {
                 piece.enPassant === false;
               }
