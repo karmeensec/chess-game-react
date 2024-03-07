3937a4cfc0b83a818865d3a09c49572c3e1685fa
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2024-03-07 11:16:15 +0100

    add possible moves for the bishops

--- src/components/referee/rules/BishopRules.ts
+++ src/components/referee/rules/BishopRules.ts
@@ -1,5 +1,9 @@
 import { Piece, Position, TeamType, samePosition } from "../../../constants";
-import { isTileEmptyOrOccupiedByEnemy, isTileOccupied } from "./GeneralRules";
+import {
+  isTileEmptyOrOccupiedByEnemy,
+  isTileOccupied,
+  isTileOccupiedByEnemy,
+} from "./GeneralRules";
 
 export const bishopMovement = (
   initialPosition: Position,
@@ -97,4 +101,69 @@ export const bishopMovement = (
   }
 
   return false;
+};
+
+export const getPossibleBishopMoves = (
+  bishop: Piece,
+  boardState: Piece[]
+): Position[] => {
+  const possibleMoves: Position[] = [];
+
+  // Upper Right
+  for (let i = 1; i < 8; i++) {
+    const destination = { x: bishop.position.x + i, y: bishop.position.y + i };
+
+    if (!isTileOccupied(destination, boardState)) {
+      possibleMoves.push(destination);
+    } else if (isTileOccupiedByEnemy(destination, boardState, bishop.team)) {
+      possibleMoves.push(destination);
+      break;
+    } else {
+      break;
+    }
+  }
+
+  // Bottom Right
+  for (let i = 1; i < 8; i++) {
+    const destination = { x: bishop.position.x + i, y: bishop.position.y - i };
+
+    if (!isTileOccupied(destination, boardState)) {
+      possibleMoves.push(destination);
+    } else if (isTileOccupiedByEnemy(destination, boardState, bishop.team)) {
+      possibleMoves.push(destination);
+      break;
+    } else {
+      break;
+    }
+  }
+
+  // Bottom Left
+  for (let i = 1; i < 8; i++) {
+    const destination = { x: bishop.position.x - i, y: bishop.position.y - i };
+
+    if (!isTileOccupied(destination, boardState)) {
+      possibleMoves.push(destination);
+    } else if (isTileOccupiedByEnemy(destination, boardState, bishop.team)) {
+      possibleMoves.push(destination);
+      break;
+    } else {
+      break;
+    }
+  }
+
+  // Upper Left
+  for (let i = 1; i < 8; i++) {
+    const destination = { x: bishop.position.x - i, y: bishop.position.y + i };
+
+    if (!isTileOccupied(destination, boardState)) {
+      possibleMoves.push(destination);
+    } else if (isTileOccupiedByEnemy(destination, boardState, bishop.team)) {
+      possibleMoves.push(destination);
+      break;
+    } else {
+      break;
+    }
+  }
+
+  return possibleMoves;
 };
