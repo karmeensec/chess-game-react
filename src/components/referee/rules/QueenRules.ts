e9d55f40bfebb607933ba677c5417689cf0d875d
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2024-03-07 11:39:41 +0100

    add possible moves for the queen

--- src/components/referee/rules/QueenRules.ts
+++ src/components/referee/rules/QueenRules.ts
@@ -1,5 +1,9 @@
 import { Piece, Position, TeamType, samePosition } from "../../../constants";
-import { isTileEmptyOrOccupiedByEnemy, isTileOccupied } from "./GeneralRules";
+import {
+  isTileEmptyOrOccupiedByEnemy,
+  isTileOccupied,
+  isTileOccupiedByEnemy,
+} from "./GeneralRules";
 
 export const queenMovement = (
   initialPosition: Position,
@@ -39,4 +43,129 @@ export const queenMovement = (
     }
   }
   return false;
+};
+
+export const getPossibleQueenMoves = (
+  queen: Piece,
+  boardState: Piece[]
+): Position[] => {
+  const possibleMoves: Position[] = [];
+
+  // Top movement
+
+  for (let i = 1; i < 8; i++) {
+    const destination = { x: queen.position.x, y: queen.position.y + i };
+
+    if (!isTileOccupied(destination, boardState)) {
+      possibleMoves.push(destination);
+    } else if (isTileOccupiedByEnemy(destination, boardState, queen.team)) {
+      possibleMoves.push(destination);
+      break;
+    } else {
+      break;
+    }
+  }
+
+  // Bottom movement
+
+  for (let i = 1; i < 8; i++) {
+    const destination = { x: queen.position.x, y: queen.position.y - i };
+
+    if (!isTileOccupied(destination, boardState)) {
+      possibleMoves.push(destination);
+    } else if (isTileOccupiedByEnemy(destination, boardState, queen.team)) {
+      possibleMoves.push(destination);
+      break;
+    } else {
+      break;
+    }
+  }
+
+  // Left movement
+
+  for (let i = 1; i < 8; i++) {
+    const destination = { x: queen.position.x - i, y: queen.position.y };
+
+    if (!isTileOccupied(destination, boardState)) {
+      possibleMoves.push(destination);
+    } else if (isTileOccupiedByEnemy(destination, boardState, queen.team)) {
+      possibleMoves.push(destination);
+      break;
+    } else {
+      break;
+    }
+  }
+
+  // Right movement
+
+  for (let i = 1; i < 8; i++) {
+    const destination = { x: queen.position.x + i, y: queen.position.y };
+
+    if (!isTileOccupied(destination, boardState)) {
+      possibleMoves.push(destination);
+    } else if (isTileOccupiedByEnemy(destination, boardState, queen.team)) {
+      possibleMoves.push(destination);
+      break;
+    } else {
+      break;
+    }
+  }
+
+  // Upper Right
+  for (let i = 1; i < 8; i++) {
+    const destination = { x: queen.position.x + i, y: queen.position.y + i };
+
+    if (!isTileOccupied(destination, boardState)) {
+      possibleMoves.push(destination);
+    } else if (isTileOccupiedByEnemy(destination, boardState, queen.team)) {
+      possibleMoves.push(destination);
+      break;
+    } else {
+      break;
+    }
+  }
+
+  // Bottom Right
+  for (let i = 1; i < 8; i++) {
+    const destination = { x: queen.position.x + i, y: queen.position.y - i };
+
+    if (!isTileOccupied(destination, boardState)) {
+      possibleMoves.push(destination);
+    } else if (isTileOccupiedByEnemy(destination, boardState, queen.team)) {
+      possibleMoves.push(destination);
+      break;
+    } else {
+      break;
+    }
+  }
+
+  // Bottom Left
+  for (let i = 1; i < 8; i++) {
+    const destination = { x: queen.position.x - i, y: queen.position.y - i };
+
+    if (!isTileOccupied(destination, boardState)) {
+      possibleMoves.push(destination);
+    } else if (isTileOccupiedByEnemy(destination, boardState, queen.team)) {
+      possibleMoves.push(destination);
+      break;
+    } else {
+      break;
+    }
+  }
+
+  // Upper Left
+  for (let i = 1; i < 8; i++) {
+    const destination = { x: queen.position.x - i, y: queen.position.y + i };
+
+    if (!isTileOccupied(destination, boardState)) {
+      possibleMoves.push(destination);
+    } else if (isTileOccupiedByEnemy(destination, boardState, queen.team)) {
+      possibleMoves.push(destination);
+      break;
+    } else {
+      break;
+    }
+  }
+
+  return possibleMoves;
 };
