5934504b387340048859c906fc47e7fdada35a66
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2024-03-08 21:05:19 +0100

    move enums to the types file

---
+++ src/types.ts
@@ -0,0 +1,13 @@
+export enum PieceType {
+  PAWN = "pawn",
+  ROOK = "rook",
+  BISHOP = "bishop",
+  KNIGHT = "knight",
+  QUEEN = "queen",
+  KING = "king",
+}
+
+export enum TeamType {
+  OPPONENT = "b",
+  MY = "w",
+}
