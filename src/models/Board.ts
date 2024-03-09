b7528935be3ef0e2fe8f8a66bbef9348c4556256
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2024-03-09 19:46:49 +0100

    add possibility for the king not to move when in danger

--- src/models/Board.ts
+++ src/models/Board.ts
@@ -21,6 +21,77 @@ export class Board {
     for (const piece of this.pieces) {
       piece.possibleMoves = this.getValidMoves(piece, this.pieces);
     }
+
+    this.checkKingMoves();
+  }
+
+  checkKingMoves() {
+    const king = this.pieces.find(
+      (p) => p.isKing && p.team === TeamType.OPPONENT
+    );
+
+    if (king?.possibleMoves === undefined) return;
+
+    for (const move of king.possibleMoves) {
+      const simulatedBoard = this.clone();
+
+      const pieceAtDestination = simulatedBoard.pieces.find((p) =>
+        p.samePosition(move)
+      );
+
+      if (pieceAtDestination !== undefined) {
+        simulatedBoard.pieces = simulatedBoard.pieces.filter(
+          (p) => !p.samePosition(move)
+        );
+      }
+
+      const simulatedKing = simulatedBoard.pieces.find(
+        (p) => p.isKing && p.team === TeamType.OPPONENT
+      );
+
+      if (simulatedKing === undefined) continue;
+
+      simulatedKing.position = move;
+
+      for (const enemy of simulatedBoard.pieces.filter(
+        (p) => p.team === TeamType.MY
+      )) {
+        enemy.possibleMoves = simulatedBoard.getValidMoves(
+          enemy,
+          simulatedBoard.pieces
+        );
+      }
+      let safe = true;
+
+      for (const p of simulatedBoard.pieces) {
+        if (p.team === TeamType.OPPONENT) continue;
+
+        if (p.isPawn) {
+          const possiblePawnMoves = simulatedBoard.getValidMoves(
+            p,
+            simulatedBoard.pieces
+          );
+
+          if (
+            possiblePawnMoves?.some(
+              (ppm) => ppm.x !== p.position.x && ppm.samePosition(move)
+            )
+          ) {
+            safe = false;
+            break;
+          }
+        } else if (p.possibleMoves?.some((p) => p.samePosition(move))) {
+          safe = false;
+          break;
+        }
+      }
+
+      if (!safe) {
+        king.possibleMoves = king.possibleMoves?.filter(
+          (m) => !m.samePosition(move)
+        );
+      }
+    }
   }
 
   getValidMoves = (piece: Piece, boardState: Piece[]): Position[] => {
@@ -119,9 +190,6 @@ export class Board {
 
   clone(): Board {
     const clonedPieces = this.pieces.map((p) => p.clone());
-
     return new Board(clonedPieces);
-
-    // return new Board(this.pieces.map((p) => p.clone()));
   }
 }
