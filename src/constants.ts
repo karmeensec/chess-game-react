6f8efa31d2bd80d5cfc39ef0417ab51d2c4a1137
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2024-03-09 09:24:25 +0100

    add pawns to constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,5 @@
 import { Piece, Position } from "./models";
+import { Pawn } from "./models/Pawn";
 import { PieceType, TeamType } from "./types";
 
 export const HORIZONTAL_AXIS = ["a", "b", "c", "d", "e", "f", "g", "h"];
@@ -12,42 +13,51 @@ export const initialBoardState: Piece[] = [
   // Rooks
 
   new Piece(new Position(0, 7), PieceType.ROOK, TeamType.OPPONENT),
-
   new Piece(new Position(7, 7), PieceType.ROOK, TeamType.OPPONENT),
-
   new Piece(new Position(0, 0), PieceType.ROOK, TeamType.MY),
-
   new Piece(new Position(7, 0), PieceType.ROOK, TeamType.MY),
 
   // Knights
 
   new Piece(new Position(1, 7), PieceType.KNIGHT, TeamType.OPPONENT),
-
   new Piece(new Position(6, 7), PieceType.KNIGHT, TeamType.OPPONENT),
-
   new Piece(new Position(1, 0), PieceType.KNIGHT, TeamType.MY),
-
   new Piece(new Position(6, 0), PieceType.KNIGHT, TeamType.MY),
 
   // Bishops
 
   new Piece(new Position(2, 7), PieceType.BISHOP, TeamType.OPPONENT),
-
   new Piece(new Position(5, 7), PieceType.BISHOP, TeamType.OPPONENT),
-
   new Piece(new Position(2, 0), PieceType.BISHOP, TeamType.MY),
-
   new Piece(new Position(5, 0), PieceType.BISHOP, TeamType.MY),
 
   // Queen
 
   new Piece(new Position(3, 7), PieceType.QUEEN, TeamType.OPPONENT),
-
   new Piece(new Position(3, 0), PieceType.QUEEN, TeamType.MY),
 
   // King
 
   new Piece(new Position(4, 7), PieceType.KING, TeamType.OPPONENT),
-
   new Piece(new Position(4, 0), PieceType.KING, TeamType.MY),
+
+  // Pawns
+
+  new Pawn(new Position(0, 6), TeamType.OPPONENT, false),
+  new Pawn(new Position(1, 6), TeamType.OPPONENT, false),
+  new Pawn(new Position(2, 6), TeamType.OPPONENT, false),
+  new Pawn(new Position(3, 6), TeamType.OPPONENT, false),
+  new Pawn(new Position(4, 6), TeamType.OPPONENT, false),
+  new Pawn(new Position(5, 6), TeamType.OPPONENT, false),
+  new Pawn(new Position(6, 6), TeamType.OPPONENT, false),
+  new Pawn(new Position(7, 6), TeamType.OPPONENT, false),
+
+  new Pawn(new Position(0, 1), TeamType.MY, false),
+  new Pawn(new Position(1, 1), TeamType.MY, false),
+  new Pawn(new Position(2, 1), TeamType.MY, false),
+  new Pawn(new Position(3, 1), TeamType.MY, false),
+  new Pawn(new Position(4, 1), TeamType.MY, false),
+  new Pawn(new Position(5, 1), TeamType.MY, false),
+  new Pawn(new Position(6, 1), TeamType.MY, false),
+  new Pawn(new Position(7, 1), TeamType.MY, false),
 ];
